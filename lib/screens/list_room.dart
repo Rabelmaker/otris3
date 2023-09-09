e351d585e9b23a057a7fb0d0059c5effc87b294c
akbar <akbar.maulana26061999@gmail.com>
2023-09-09 19:57:37 +0700

    buat list_room.dart

---
+++ lib/screens/list_room.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ListRoom extends StatefulWidget {
+  const ListRoom({super.key});
+
+  @override
+  State<ListRoom> createState() => _ListRoomState();
+}
+
+class _ListRoomState extends State<ListRoom> {
+  Color greenland = const Color(0xff0E5F00);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: greenland,
+        title: Text("List Room"),
+        actions: [
+         IconButton(onPressed: (){}, icon: const Icon(Icons.history))
+        ],
+      ),
+    );
+  }
+}
