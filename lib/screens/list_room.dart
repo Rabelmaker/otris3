b97b3e2b06a9c999d9b9245953c389940c541ced
Akbar Maulana <akbar.maulana26061999@gmail.com>
2023-09-11 16:33:13 +0700

    masuk

--- lib/screens/list_room.dart
+++ lib/screens/list_room.dart
@@ -13,18 +13,84 @@ class _ListRoomState extends State<ListRoom> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: greenland,
-        title: Text("List Room"),
+        title: const Text("List Room"),
         actions: [
-         IconButton(onPressed: (){}, icon: const Icon(Icons.history))
+          IconButton(onPressed: () {}, icon: const Icon(Icons.history))
         ],
       ),
       body: SafeArea(
-        child: ListView(
-          children: [
-
-          ],
-        )
-      ),
+          child: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/otris.jpg"),
+              fit: BoxFit.cover,
+            )),
+          ),
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                  Colors.black.withOpacity(0.9),
+                  Colors.black.withOpacity(0.7),
+                ])),
+          ),
+          Container(
+            padding: const EdgeInsets.all(100),
+            width: double.infinity,
+            height: double.infinity,
+            child: Column(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      _itemCard("loginbackground.png"),
+                      _itemCard("loginbackground.png")
+                    ],
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      _itemCard("loginbackground.png"),
+                      _itemCard("loginbackground.png")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )),
     );
+  }
+
+  Widget _itemCard(String gambar) {
+    return Flexible(
+        flex: 1,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/$gambar"), fit: BoxFit.cover)),
+          child: InkWell(
+            onTap: () {},
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              color: Colors.white.withOpacity(0.6),
+            ),
+          ),
+        ));
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -34,152 +34,166 @@ class _LoginState extends State<Login> {
                     begin: Alignment.centerLeft,
                     end: Alignment.centerRight,
                     colors: [
-                  Colors.black.withOpacity(0.85),
-                  Colors.black.withOpacity(0.7),
-                  Colors.transparent,
+                  Colors.black.withOpacity(0.9),
+                  Colors.black.withOpacity(0.75),
+                  // Colors.transparent,
                 ])),
           ),
           Center(
             child: SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.all(32.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          height: 70,
-                          width: 70,
-                          decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage("assets/logo_otris.png"))),
-                        ),
-                        const SizedBox(
-                          width: 16,
-                        ),
-                        const Text(
-                          "Login OTRIS",
-                          style: TextStyle(
-                            fontSize: 36,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 400),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage("assets/logo_otris.png"))),
                           ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    const Text(
-                      "Enter Username & Password to Access your Account",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    const Text(
-                      "Email",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Container(
-                      width: 650,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          color: greenland,
-                          width: 3.0,
-                        ),
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: TextFormField(
-                          maxLines: 1,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
+                          const SizedBox(
+                            width: 16,
                           ),
-                          controller: userController,
+                          const Text(
+                            "Login OTRIS",
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        "Enter Username & Password to Access your Account",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    const Text(
-                      "Password",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
+                      const SizedBox(
+                        height: 40,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Container(
-                      width: 650,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          color: greenland,
-                          width: 3.0,
-                        ),
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          obscureText: true,
-                          maxLines: 1,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Email",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
                           ),
-                          controller: passwordController,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 48,
-                    ),
-                    Container(
-                        width: 650,
-                        height: 50,
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all<Color>(greenland),
-                              shape:
-                                  MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16.0),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Container(
+                            width: 650,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: greenland,
+                                width: 3.0,
+                              ),
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: TextFormField(
+                                maxLines: 1,
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
                                 ),
+                                controller: userController,
                               ),
                             ),
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontSize: 24, fontWeight: FontWeight.bold),
-                            ))),
-                  ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Password",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Container(
+                            width: 650,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: greenland,
+                                width: 3.0,
+                              ),
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                obscureText: true,
+                                maxLines: 1,
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                ),
+                                controller: passwordController,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 48,
+                      ),
+                      Container(
+                          width: 650,
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(greenland),
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ))),
+                    ],
+                  ),
                 ),
               ),
             ),
           )
         ],
-      )
-      ),
+      )),
     );
   }
 }
